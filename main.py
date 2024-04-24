7d34c787424af8ff98220a4658f2255298c53723
danishkhan216 <147950036+danishkhan216@users.noreply.github.com>
2024-04-25 00:41:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+# Import functions for data preprocessing & data preparation
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer, LancasterStemmer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
+import string
+from string import punctuation
+import nltk
+import re
 from textblob import TextBlob
 import pandas as pd
 import streamlit as st
 import cleantext
-
-
-st.header('Sentiment Analysis')
+st.header('Comment Analysis')
 with st.expander('Analyze Text'):
     text = st.text_input('Text here: ')
     if text:
         blob = TextBlob(text)
         st.write('Polarity: ', round(blob.sentiment.polarity,2))
         st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
-
-
-    pre = st.text_input('Clean Text: ')
-    if pre:
+def analyze_polarity_subjectivity(text):
+    # Get polarity and subjectivity
+    polarity = blob.sentiment.polarity
+    subjectivity = blob.sentiment.subjectivity
+    
+    return polarity, subjectivity
+ 
+pre = st.text_input('Clean Text: ')
+if pre:
         st.write(cleantext.clean(pre, clean_all= False, extra_spaces=True,
                                  stopwords=True ,lowercase=True ,numbers=True , punct=True))
 
@@ -27,22 +49,21 @@ with st.expander('Analyze CSV'):
 
 #
     def analyze(x):
-        if x >= 0.5:
-            return 'Positive'
-        elif x <= -0.5:
+        if x < 0:
             return 'Negative'
-        else:
+        elif x ==0:
             return 'Neutral'
+        else:
+            return 'Posistive'
 
-#
-    if upl:
+
+if upl:
         df = pd.read_excel(upl)
-        del df['Unnamed: 0']
-        df['score'] = df['tweets'].apply(score)
+        df['score'] = df['Comment'].apply(score)
         df['analysis'] = df['score'].apply(analyze)
         st.write(df.head(10))
 
-        @st.cache
+        @st.cache_data 
         def convert_df(df):
             # IMPORTANT: Cache the conversion to prevent computation on every rerun
             return df.to_csv().encode('utf-8')
@@ -52,6 +73,8 @@ with st.expander('Analyze CSV'):
         st.download_button(
             label="Download data as CSV",
             data=csv,
-            file_name='sentiment.csv',
+            file_name='comments.csv',
             mime='text/csv',
         )
+        
+       
